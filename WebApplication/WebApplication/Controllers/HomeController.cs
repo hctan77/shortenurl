442390ff27a60c716e1aba741bc1f14eda9441ce
han chong <han_chong@rocketmail.com>
2019-09-07 19:40:41 +0800

    Remove unused codes.

--- WebApplication/WebApplication/Controllers/HomeController.cs
+++ WebApplication/WebApplication/Controllers/HomeController.cs
@@ -1,32 +1,13 @@
-﻿using System.Diagnostics;
-using System.Net;
-using Microsoft.AspNetCore.Mvc;
-using WebApplication.Models;
-
-namespace WebApplication.Controllers
+﻿namespace WebApplication.Controllers
 {
+    using System.Diagnostics;
+    using System.Net;
+    using Microsoft.AspNetCore.Mvc;
+    using WebApplication.Models;
+
     public class HomeController : Controller
     {
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
