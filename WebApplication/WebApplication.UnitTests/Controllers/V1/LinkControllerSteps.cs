425e7a40f6dd4117e8524abb5633c0afe870e210
han chong <han_chong@rocketmail.com>
2019-09-07 20:45:23 +0800

    Add unit tests for link controller

---
+++ WebApplication/WebApplication.UnitTests/Controllers/V1/LinkControllerSteps.cs
@@ -0,0 +1,91 @@
+﻿namespace WebApplication.UnitTests.Controllers.V1
+{
+    using FluentAssertions;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Moq;
+    using System;
+    using System.Net;
+    using System.Threading.Tasks;
+    using WebApplication.Configuration;
+    using WebApplication.Controllers.V1;
+    using WebApplication.Core;
+
+    internal sealed class LinkControllerSteps
+    {
+        private readonly LinkController controller;
+        private readonly Mock<IRepository> mockRepository = new Mock<IRepository>();
+        private readonly Mock<IShortenUrlGenerator> mockUrlGenerator = new Mock<IShortenUrlGenerator>();
+
+        private object result;
+
+        public LinkControllerSteps()
+        {
+            var controllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+
+            this.controller = new LinkController(mockRepository.Object, mockUrlGenerator.Object, new ApiOptions { AddMaxRetry = 2 })
+            {
+                ControllerContext = controllerContext
+            };
+        }
+
+        public LinkControllerSteps GivenIHaveShortUrl(string shortUrl, Uri longUrl)
+        {
+            mockRepository.Setup(x => x.GetAsync(shortUrl)).ReturnsAsync(longUrl);
+            return this;
+        }
+
+        public LinkControllerSteps GivenISetupUrlGeneratorToReturn(string shortUrl)
+        {
+            mockUrlGenerator.Setup(x => x.GetRandomUrl(It.IsAny<Uri>())).Returns(shortUrl);
+            return this;
+        }
+
+        public LinkControllerSteps GivenISetupRepositoryAdd(string shortUrl, Uri longUrl, bool added)
+        {
+            mockRepository.Setup(x => x.AddAsync(shortUrl, longUrl)).ReturnsAsync(added);
+            return this;
+        }
+
+        public async Task WhenICallShortenUrlAsync(Uri url)
+        {
+            this.result = await this.controller.ShortenAsync(url).ConfigureAwait(false);
+        }
+
+        public async Task WhenICallGetLongUrlAsync(string shortUrl)
+        {
+            this.result = await this.controller.GetLongUrlAsync(shortUrl).ConfigureAwait(false);
+        }
+
+        public LinkControllerSteps ThenShortenUrlShouldBe(string expected)
+        {
+            var url = this.result as ActionResult<string>;
+            url.Value.Should().Be(expected);
+
+            return this;
+        }
+
+        public LinkControllerSteps ThenLongUrlShouldBe(Uri expected)
+        {
+            var url = this.result as ActionResult<Uri>;
+            url.Value.Should().Be(expected);
+
+            return this;
+        }
+
+        public LinkControllerSteps ThenStatusCodeShouldBe(HttpStatusCode expected)
+        {
+            this.controller.Response.StatusCode.Should().Be((int)expected);
+            return this;
+        }
+
+        public LinkControllerSteps ThenAddToRepositoryShouldBeCalled(int times)
+        {
+            mockRepository.Verify(x => x.AddAsync(It.IsAny<string>(), It.IsAny<Uri>()), Times.Exactly(times));
+            return this;
+        }
+    }
+}
---
+++ WebApplication/WebApplication.UnitTests/Controllers/V1/LinkControllerTests.cs
@@ -0,0 +1,93 @@
+﻿namespace WebApplication.UnitTests.Controllers.V1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public sealed class LinkControllerTests
+    {
+        private readonly LinkControllerSteps steps = new LinkControllerSteps();
+        private readonly Uri sampleUrl = new Uri("http://www.example.com");
+
+        [Fact]
+        public async Task ShortenAsync_NullUrl_BadRequest()
+        {
+            await this.steps
+                .WhenICallShortenUrlAsync(null).ConfigureAwait(false);
+
+            this.steps
+                .ThenStatusCodeShouldBe(HttpStatusCode.BadRequest);
+        }
+
+        [Fact]
+        public async Task ShortenAsync_FailedToAdd_InternalServerError()
+        {
+            await this.steps
+                .GivenISetupRepositoryAdd("abc", this.sampleUrl, false)
+                .GivenISetupUrlGeneratorToReturn("abc")
+                .WhenICallShortenUrlAsync(this.sampleUrl).ConfigureAwait(false);
+
+            this.steps
+                .ThenAddToRepositoryShouldBeCalled(2)
+                .ThenStatusCodeShouldBe(HttpStatusCode.InternalServerError);
+        }
+
+        [Fact]
+        public async Task ShortenAsync_AddSuccessful_ReturnsShortUrl()
+        {
+            await this.steps
+                .GivenISetupRepositoryAdd("abc", this.sampleUrl, true)
+                .GivenISetupUrlGeneratorToReturn("abc")
+                .WhenICallShortenUrlAsync(this.sampleUrl).ConfigureAwait(false);
+
+            this.steps
+                .ThenAddToRepositoryShouldBeCalled(1)
+                .ThenStatusCodeShouldBe(HttpStatusCode.OK)
+                .ThenShortenUrlShouldBe("abc");
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public async Task GetLongUrlAsync_InvalidShortUrl_BadRequest(string shortUrl)
+        {
+            await this.steps
+                .WhenICallGetLongUrlAsync(shortUrl).ConfigureAwait(false);
+
+            this.steps
+                .ThenStatusCodeShouldBe(HttpStatusCode.BadRequest);
+        }
+
+        [Fact]
+        public async Task GetLongUrlAsync_ValidShortUrl_ReturnsLongUrl()
+        {
+            var shortUrl = "abc";
+            var longUrl = new Uri("http://www.example.com");
+
+            await this.steps
+                .GivenIHaveShortUrl(shortUrl, longUrl)
+                .WhenICallGetLongUrlAsync(shortUrl).ConfigureAwait(false);
+
+            this.steps
+                .ThenStatusCodeShouldBe(HttpStatusCode.OK)
+                .ThenLongUrlShouldBe(longUrl);
+        }
+
+        [Fact]
+        public async Task GetLongUrlAsync_ShortUrlNotFound_NotFound()
+        {
+            var shortUrl = "abc";
+
+            await this.steps
+                .GivenIHaveShortUrl(shortUrl, null)
+                .WhenICallGetLongUrlAsync(shortUrl).ConfigureAwait(false);
+
+            this.steps
+                .ThenStatusCodeShouldBe(HttpStatusCode.NotFound);
+        }
+    }
+}
