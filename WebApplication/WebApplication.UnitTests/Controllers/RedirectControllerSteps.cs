3338f162fb129ff75034abe7527b26c57d48d83f
han chong <han_chong@rocketmail.com>
2019-09-07 21:01:53 +0800

    Add unit test for redirect controller

---
+++ WebApplication/WebApplication.UnitTests/Controllers/RedirectControllerSteps.cs
@@ -0,0 +1,63 @@
+﻿namespace WebApplication.UnitTests.Controllers
+{
+    using FluentAssertions;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Moq;
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Text;
+    using System.Threading.Tasks;
+    using WebApplication.Controllers;
+    using WebApplication.Core;
+
+    internal sealed class RedirectControllerSteps
+    {
+        private readonly RedirectController controller;
+        private readonly Mock<IRepository> mockRepository = new Mock<IRepository>();
+
+        private object result;
+
+        public RedirectControllerSteps()
+        {
+            var controllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+
+            this.controller = new RedirectController(mockRepository.Object)
+            {
+                ControllerContext = controllerContext
+            };
+        }
+
+        public RedirectControllerSteps GivenIHaveShortUrl(string shortUrl, Uri longUrl)
+        {
+            mockRepository.Setup(x => x.GetAsync(shortUrl)).ReturnsAsync(longUrl);
+            return this;
+        }
+
+        public async Task WhenICallIndexAsync(string shortUrl)
+        {
+            this.result = await this.controller.IndexAsync(shortUrl).ConfigureAwait(false);
+        }
+
+        public RedirectControllerSteps ThenResultShouldBeNotFound()
+        {
+            var notFoundResult = this.result as NotFoundResult;
+            notFoundResult.Should().NotBeNull();
+
+            return this;
+        }
+
+        public RedirectControllerSteps ThenResultShouldBeRedirect(Uri expected)
+        {
+            var redirectResult = this.result as RedirectResult;
+            redirectResult.Should().NotBeNull();
+            redirectResult.Url.Should().Be(expected.ToString());
+
+            return this;
+        }
+    }
+}
---
+++ WebApplication/WebApplication.UnitTests/Controllers/RedirectControllerTests.cs
@@ -0,0 +1,48 @@
+﻿namespace WebApplication.UnitTests.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public sealed class RedirectControllerTests
+    {
+        private readonly RedirectControllerSteps steps = new RedirectControllerSteps();
+
+        [Fact]
+        public async Task IndexAsync_NullShortUrl_NotFound()
+        {
+            await this.steps
+                .WhenICallIndexAsync(null).ConfigureAwait(false);
+
+            this.steps
+                .ThenResultShouldBeNotFound();
+        }
+
+        [Fact]
+        public async Task IndexAsync_NonExistsShortUrl_NotFound()
+        {
+            await this.steps
+                .GivenIHaveShortUrl("abc", null)
+                .WhenICallIndexAsync("abc").ConfigureAwait(false);
+
+            this.steps
+                .ThenResultShouldBeNotFound();
+        }
+
+        [Fact]
+        public async Task IndexAsync_ExistingShortUrl_Redirect()
+        {
+            var longUrl = new Uri("http://www.example.com");
+
+            await this.steps
+                .GivenIHaveShortUrl("abc", longUrl)
+                .WhenICallIndexAsync("abc").ConfigureAwait(false);
+
+            this.steps
+                .ThenResultShouldBeRedirect(longUrl);
+        }
+    }
+}
