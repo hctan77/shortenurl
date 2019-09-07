45a3aac4a5f3e694d4000a465deadfb1f1a819c6
han chong <han_chong@rocketmail.com>
2019-09-07 22:31:55 +0800

    Add unit test for home controller.

---
+++ WebApplication/WebApplication.UnitTests/Controllers/HomeControllerSteps.cs
@@ -0,0 +1,49 @@
+﻿namespace WebApplication.UnitTests.Controllers
+{
+    using FluentAssertions;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using WebApplication.Controllers;
+    using WebApplication.Models;
+
+    internal sealed class HomeControllerSteps
+    {
+        private readonly HomeController controller;
+
+        private object result;
+
+        public HomeControllerSteps()
+        {
+            var controllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+
+            this.controller = new HomeController()
+            {
+                ControllerContext = controllerContext
+            };
+        }
+
+        public HomeControllerSteps WhenIExecuteError()
+        {
+            this.result = this.controller.Error();
+            return this;
+        }
+
+        public HomeControllerSteps ThenViewResultShouldContainsErrorViewModel()
+        {
+            var viewResult = this.result as ViewResult;
+            viewResult.Should().NotBeNull();
+
+            var errorViewModel = viewResult.Model as ErrorViewModel;
+            errorViewModel.Should().NotBeNull();
+            errorViewModel.RequestId.Should().NotBeNull();
+            
+            return this;
+        }
+    }
+}
---
+++ WebApplication/WebApplication.UnitTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApplication.UnitTests.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Xunit;
+
+    public sealed class HomeControllerTests
+    {
+        private readonly HomeControllerSteps steps = new HomeControllerSteps();
+
+        [Fact]
+        public void Error_Execute_ReturnsErrorViewModel()
+        {
+            this.steps.WhenIExecuteError()
+                .ThenViewResultShouldContainsErrorViewModel();
+        }
+    }
+}
