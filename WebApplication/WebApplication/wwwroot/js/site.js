edf862579a253e05dea0681b9e64ce59148a842d
han chong <han_chong@rocketmail.com>
2019-09-08 15:06:50 +0800

    Add ui to create shorten url

--- WebApplication/WebApplication/wwwroot/js/site.js
+++ WebApplication/WebApplication/wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function isEmptyOrSpaces(str) {
+    return str === null || str.match(/^ *$/) !== null;
+}
+
+function shortenUrl() {
+    var longUrl = document.getElementById("inputLongUrl").value;
+
+    if (isEmptyOrSpaces(longUrl)) {
+        alert("Invalid long URL.");
+    }
+
+    $.ajax({
+        url: "https://shortenurl.trafficmanager.net/api/v1/link?url=" + longUrl,
+        method: "POST",
+        success: function (result) {
+            alert("Short URL generated - [" + result + "]");
+        },
+        fail: function () {
+            alert("Fail to generate short URL.");
+        }
+    });
+}
+
+$(document).ready(function () {
+    document.getElementById("buttonLongUrl").addEventListener("click", shortenUrl);
+});
